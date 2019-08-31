eb59a1b14cc7a3b92c5f0623ac54b67b984b1a8e
Baggins <neduvinci@gmail.com>
2019-08-31 13:05:25 +0100

    completed objects, on to arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -13,6 +13,7 @@ const trexObj = {
   weight: '7000kg',
   length: '12m',
   period: 'Late Cretaceous',
+  roar: roar,
 }
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 const stegyObj = {
@@ -35,20 +36,23 @@ const veloObj = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+// console.log(trexObj.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+// console.log(veloObj.diet);
 
 // How long was a stegosaurus?
-console.log();
+// console.log(stegyObj.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+// console.log(trexObj.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+function roar() {
+  return `RAWERSRARARWERSARARARRRR!`;
+}
+// console.log(trexObj.roar());
 
 
 // ==== Arrays ====
